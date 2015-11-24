ab7b9e4c6845133c9d6464ac0885bbe4bbcdba34
Shayan Eskandari <shayanqwerty@gmail.com>
2015-11-23 23:55:11 -0500

    python3 fixes

--- providers.py
+++ providers.py
@@ -13,15 +13,15 @@ SCORECHAIN_TOKEN = ""
 def get_scorechain(address):
     """
     get the json related to the address from scorechain
-    token: XB0zBhmY
     """
 
     url = "https://www.scorechain.com/api_beta/address/%s?token=%s" %(address, SCORECHAIN_TOKEN)
     try:
         score_result = requests.get(url=url)
-        ret_json = json.loads(score_result.json())
+        ret_json = score_result.json()
+        print (ret_json)
         return ret_json
 
-    except Exception:
-        print ("Scorechain failed for %s" %(address))
+    except Exception as err:
+        print ("Scorechain failed for %s: %s" %(address, err))
         return None
